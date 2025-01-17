5047d0e7387a463abb93d6c1288e282293958ca8
sree7979 <132588975+sree7979@users.noreply.github.com>
2025-01-16 20:44:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends
 import random
 import logging
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 
 app = FastAPI()
