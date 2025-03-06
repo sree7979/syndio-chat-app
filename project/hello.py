b86d622c5636c0d8e599363fdb88f11f02a26565
sree7979 <tsreeja995@gmail.com>
2025-03-05 20:38:38 -0500

    Updated schema manager and SQLAgent logic

---
+++ project/hello.py
@@ -0,0 +1,4 @@
+a = 1
+b = 2
+sum = a + b
+print(sum)  # This will print the value of sum, not the string "sum"
---
+++ project/prod/sqlagent.py
@@ -0,0 +1,4 @@
+a = 2
+b = 2
+multiply = a * b
+print(multiply)  # This will print the value of multiplication
